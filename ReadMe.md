a4129f14255a2c6337b12a9e814e18475d072a35
karimashour2 <karim.note10.l@gmail.com>
2022-12-23 17:29:03 +0200

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,56 @@
+Professional Front-End Web Development - Udacity
+Project 1 : Landing Page
+That page name is (Landing Page) contain of dynamic navigation bar based on javascript
+and 4 sections there are linked with the navigation bar so to auto-edit if the number 
+of sections is changed.
+first, i created index.itml file to build up the stucture of the page 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+
+  <body>
+
+    <header>
+        - # Landing Page
+      </nav>
+    </header>
+  
+  
+    # Section 1
+        section 1 paragraph
+  
+   
+  
+    # Section 2
+        section 2 paragraph
+  
+
+ 
+    # Section 3
+        section 3 paragraph
+  
+
+  
+    # Section 4
+        section 4 paragraph
+  
+  
+  </body>
+
+
+  <footer>
+      All rights reserved.KARIM © 2022
+  </footer>
+
+  <script src="js/app.js"></script>
+
+</html>
+
+Second, i started to style the page by creating style.css
---
+++ js/app.js
@@ -0,0 +1,59 @@
+
+// get ul element
+const navList = document.getElementById("nav-list");
+
+const sections = document.querySelectorAll('section');
+
+//loop over sections
+// document.querySelectorAll('section').forEach(section => {
+
+// another way to loop
+for(const section of sections) {
+    const title = section.querySelector('h1').innerText;
+    const li = `<li><a class="nav" href="#${section.id}">${title}</a></li>`;
+
+    navList.insertAdjacentHTML('beforeend', li);
+}
+
+// });
+// the same as normal loop
+// for(let i=0; i < sections.length; i++) {
+//     sections[i]
+// }
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelectorAll('a').forEach(a => {
+            a.classList.remove('active');
+        });
+        anchor.classList.add('active');
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.onscroll = () => {
+    let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector("a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector("a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+  };
